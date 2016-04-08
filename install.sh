fd2c4d57c44f6211e48e7a0af08201e071a9afb9
Rahul Salvi <rsalvi006@me.com>
2016-04-08 13:18:08 -0700

    better install script

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 dotfilesdir=$HOME/dotfiles
+olddotfilesdir=$HOME/dotfiles_old
 files="bash_profile vimrc tmux.conf slate.js git_template"
 
+echo "Creating old dotfiles directory $olddotfilesdir"
+mkdir -p $olddotfilesdir
+
 for file in $files; do
-    echo "Creating symlink to $HOME/.$file in $HOME"
+    echo "Moving $HOME/.$file to $olddotfilesdir/.$file"
+    mv $HOME/.$file $olddotfilesdir/.$file
+    echo "Creating symlink to $dotfilesdir/.$file in $HOME/.$file"
     ln -s $dotfilesdir/.$file $HOME/.$file
-    echo "Created symlink $HOME/.$file"
 done
+
+echo "Sourcing $HOME/.bash_profile"
+source $HOME/.bash_profile
+
+echo "Adding $HOME/.git_template to global git config as init.templatedir"
+git config --global init.templatedir '$HOME/.git_template'
