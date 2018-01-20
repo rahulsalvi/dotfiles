810d3a4efa6ec7d9a63874162726c19d5f74a84a
Rahul Salvi <rsalvi006@me.com>
2018-01-19 20:00:10 -0800

    open terminal apps with rofi

--- rofi/apps.sh
+++ rofi/apps.sh
@@ -4,13 +4,29 @@ case "$@" in
     "chrome")
         app_to_run="google-chrome-stable"
     ;;
+    "khal")
+        app_to_run="SHELL:khal"
+    ;;
+    "mutt")
+        app_to_run="SHELL:neomutt"
+    ;;
+    "ncmpcpp")
+        app_to_run="SHELL:ncmpcpp"
+    ;;
+    "topydo")
+        app_to_run="SHELL:topydo columns"
+    ;;
     "")
         echo "chrome"
         echo "discord"
         echo "keepassxc"
+        echo "khal"
+        echo "mutt"
+        echo "ncmpcpp"
         echo "pavucontrol"
         echo "steam"
         echo "termite"
+        echo "topydo"
         echo "vlc"
     ;;
     *)
@@ -18,6 +34,12 @@ case "$@" in
     ;;
 esac
 
+if [[ "$app_to_run" == SHELL:* ]]; then
+    termite -e ${app_to_run#SHELL:} >/dev/null 2>&1 &
+    exit
+fi
+
 if [[ -n "$app_to_run" ]]; then
     $app_to_run >/dev/null 2>&1 &
+    exit
 fi
