5b9eb4924e137b26c166ba0d1cb38df257d50a01
Rahul Salvi <rsalvi006@me.com>
2018-08-09 23:38:57 -0700

    styling and only return flags for cfamily files

--- ycm_extra_conf.py
+++ ycm_extra_conf.py
@@ -1,43 +1,44 @@
 # pylint: disable=R1702,R0911
 
+import logging
 import os
 import ycm_core
 
 default_flags = [
-        '-Wall',
-        '-Wextra',
-        '-Werror',
-        '-Wno-c++98-compat',
-        '-Wno-long-long',
-        '-Wno-variadic-macros',
-        '-fexceptions',
-        '-ferror-limit=10000',
-        '-DNDEBUG',
-        '-std=c++11',
-        '-xc++',
-        '-I/usr/lib/',
-        '-I/usr/include/'
+        "-Wall",
+        "-Wextra",
+        "-Werror",
+        "-Wno-c++98-compat",
+        "-Wno-long-long",
+        "-Wno-variadic-macros",
+        "-fexceptions",
+        "-ferror-limit=10000",
+        "-DNDEBUG",
+        "-std=c++11",
+        "-xc++",
+        "-I/usr/lib/",
+        "-I/usr/include/"
         ]
 
 source_exts = [
-        '.cpp',
-        '.cxx',
-        '.c'
+        ".cpp",
+        ".cxx",
+        ".c"
         ]
 
 source_dirs = [
-        'src',
-        'test'
+        "src",
+        "test"
         ]
 
 header_exts = [
-        '.h',
-        '.hxx',
-        '.hpp'
+        ".h",
+        ".hxx",
+        ".hpp"
         ]
 
 header_dirs = [
-        'include'
+        "include"
         ]
 
 def make_relative_paths_in_flags_absolute(flags, working_directory):
@@ -45,13 +46,13 @@ def make_relative_paths_in_flags_absolute(flags, working_directory):
         return list(flags)
     new_flags = []
     make_next_absolute = False
-    path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
+    path_flags = [ "-isystem", "-I", "-iquote", "--sysroot=" ]
     for flag in flags:
         new_flag = flag
 
         if make_next_absolute:
             make_next_absolute = False
-            if not flag.startswith( '/' ):
+            if not flag.startswith( "/" ):
                 new_flag = os.path.join(working_directory, flag)
 
         for path_flag in path_flags:
@@ -82,11 +83,14 @@ def find_nearest(filename, target):
     return find_nearest(parent, target)
 
 def get_compilation_db_flags_for_file(filename):
-    database_path = find_nearest(filename, 'build/compile_commands.json')
+    database_path = find_nearest(filename, "build/compile_commands.json")
     if not database_path:
-        database_path = find_nearest(filename, 'compile_commands.json')
+        database_path = find_nearest(filename, "compile_commands.json")
         if not database_path:
             return None
+    logging.info("ycm_extra_conf: found compilation database for file %s at %s",
+                 str(filename),
+                 str(database_path))
     database = ycm_core.CompilationDatabase(os.path.dirname(database_path))
     if not database:
         return None
@@ -114,8 +118,11 @@ def get_compilation_db_flags_for_file(filename):
     return None
 
 def get_include_flags_for_file(filename):
-    include_dir = find_nearest(filename, 'include')
+    include_dir = find_nearest(filename, "include")
     if include_dir:
+        logging.info("ycm_extra_conf: found include directory for file %s at %s",
+                     str(filename),
+                     str(include_dir))
         return ["-I"+include_dir]
     return None
 
@@ -123,17 +130,21 @@ def get_include_flags_for_file(filename):
 def get_flags_for_file(filename):
     compilation_db_flags = get_compilation_db_flags_for_file(filename)
     if compilation_db_flags:
+        logging.info("ycm_extra_conf: using database compilation flags for file %s", str(filename))
         return make_relative_paths_in_flags_absolute(
                 compilation_db_flags.compiler_flags_,
                 compilation_db_flags.compiler_working_dir_)
     flags = default_flags
     include_flags = get_include_flags_for_file(filename)
     if include_flags:
+        logging.info("ycm_extra_conf: using include directory flags for file %s", str(filename))
         flags = flags + include_flags
     return flags
 
 def Settings(**kwargs):
-    return {
-            'flags': get_flags_for_file(kwargs['filename']),
-            'do_cache': True
-            }
+    if kwargs["language"] == "cfamily":
+        return {
+                "flags": get_flags_for_file(kwargs["filename"]),
+                "do_cache": True
+                }
+    return None
