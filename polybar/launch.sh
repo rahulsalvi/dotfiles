628509c17ea95354f3c04768f648b97d0a4258c6
Rahul Salvi <rsalvi006@me.com>
2020-03-31 10:57:07 -0700

    clean up polybar config a bit

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,11 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 
 killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar bar0 &
-polybar bar1 &
-polybar bar2 &
+if [[ "$HOST" == "Kyogre" ]]; then
+    MONITOR=HDMI-A-0 polybar bar0 >/dev/null 2>&1 &
+    sleep 2
+    MONITOR=DisplayPort-0 polybar bar0 >/dev/null 2>&1 &
+    # MONITOR=HDMI-A-1 polybar bar0 >/dev/null 2>&1 &
+fi
 
 echo "Polybar started"
