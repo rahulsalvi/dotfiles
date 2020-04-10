f2b70ffd0c8ae11e52c138cfbd14e7d7133d347a
Rahul Salvi <rsalvi006@me.com>
2020-04-10 13:24:05 -0700

    update polybar config to have set tray position at launch

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,14 +5,14 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if [[ "$HOST" == "Kyogre" ]]; then
-    MONITOR="HDMI-A-0" \
+    POLYBAR_MONITOR="HDMI-A-0" \
     polybar bar0 >/dev/null 2>&1 &
-    sleep 2
-    MONITOR="DisplayPort-0" \
+    POLYBAR_MONITOR="DisplayPort-0" \
+    POLYBAR_TRAY_POS="none" \
     polybar bar0 >/dev/null 2>&1 &
-    # MONITOR=HDMI-A-1 polybar bar0 >/dev/null 2>&1 &
+    # POLYBAR_MONITOR=HDMI-A-1 polybar bar0 >/dev/null 2>&1 &
 elif [[ "$HOST" ==  "darkrai" ]]; then
-    MONITOR="eDP-1" \
+    POLYBAR_MONITOR="eDP-1" \
     POLYBAR_HEIGHT="60" \
     POLYBAR_MODULES_RIGHT="openvpn memory cpu temperature battery date powermenu" \
     POLYBAR_TRAY_MAXSIZE="38" \
